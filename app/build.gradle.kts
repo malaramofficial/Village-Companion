9b1a45747fdc7bd349836dbd07ca662d820a6e7f
malaramofficial <malaramofficial@gmail.com>
2026-09-14 09:06:51 +0530

    Bump app version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.malaramofficial.villagecompanion"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures { compose = true }
